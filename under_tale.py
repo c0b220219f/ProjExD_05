48c6f143cfec92e597d5ae42be2340b118e533b6
c0b22021 <c0b220219f@edu.teu.ac.jp>
2023-11-05 12:41:18 +0900

    コメント追加

--- under_tale.py
+++ under_tale.py
@@ -9,7 +9,11 @@ WIDTH = 800
 HEIGHT = 500
 
 class Explanation(pg.sprite.Sprite):
+    """説明文の表示をするExplanationクラス"""
     def __init__(self, str_, x=250, y=50):
+        """初期化メソッド
+            引数にstr_:表示する文字列
+            x,y：表示するsurfaceの大きさ"""
         self.str = str_
         self.font = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
         self.color = (255, 255, 255)
@@ -19,10 +23,18 @@ class Explanation(pg.sprite.Sprite):
         self.surface = pg.Surface((x, y))
 
     def blit(self, screen, xy:tuple):
+        """surfaceをblitするメソッド
+            引数 screen:表示するscreen
+            xy : surfaceを表示する座標タプル"""
         self.xy = xy
         screen.blit(self.surface, xy)
 
     def update(self, screen:pg.Surface, flag=True, lst=[0,0]):
+        """文字列の表示を更新するメソッド
+            引数 screen : 表示するscreen
+            flag : 文字列を表示するか否か表示するときFalse 初期値True
+            lst : surfaceにblitする座標 初期値[0,0]
+            """
         if flag:
             self.img = self.font.render("", True, self.color)
         else:
@@ -30,6 +42,9 @@ class Explanation(pg.sprite.Sprite):
         self.surface.blit(self.img, lst)
 
     def color_update(self, color:tuple):
+        """文字色の更新をする
+            引数 color : 色の情報を入れたタプル
+        """
         self.color = color
     
     
@@ -47,10 +62,15 @@ class Command(pg.sprite.Sprite):
         self.rct.center = x, y
 
     def update(self, screen:pg.Surface):
+        """ 画面表示の更新
+            引数 screen : 表示するscreen"""
         self.img = self.font.render(f"{self.name}", True, self.color)
         screen.blit(self.img, self.rct)
 
     def color_update(self, color:tuple):
+        """文字色の更新をする
+            引数 color : 色の情報を入れたタプル
+        """
         self.color = color
 
 
@@ -71,20 +91,20 @@ def main():
     item = Command("ITEM", 470, 480)
     mercy = Command("MERCY", 670, 480)
     mode = "standard" # modeの初期化
-    cmd_select = 0
+    cmd_select = 0 # コマンドの選択情報
     
+    # 説明文関係
     fight_ex = Explanation("たたかうFight")
     ex_2 = Explanation("")
-    item_ex = Explanation("item")
-    ex_select = 0
-    flag = False
-    run_point = 0
+    ex_select = 0 # 説明文の選択状況
+    flag = False # ゲームを終わらせるかどうか
+    run_point = 0 # 逃がすのに必要なポイント
 
     while True:
         screen.blit(sikaku1, (200, 200))
         fight_ex.blit(screen,(230, 230))
         ex_2.blit(screen,(230, 280))
-        # item_ex.blit(screen,(230, 230))
+        
         for event in pg.event.get():
             if event.type == pg.KEYDOWN and event.key == pg.K_RIGHT and mode == "standard":
                 if cmd_select == 3:
@@ -97,6 +117,7 @@ def main():
                 else:
                     cmd_select -= 1
             if event.type == pg.KEYDOWN and event.key == pg.K_RETURN:
+                # コマンドの選択
                 if mode == "standard":
                     if cmd_select == 0:
                         mode = "FIGHT"
@@ -129,11 +150,13 @@ def main():
                         ex_2.update(screen, flag=False)
                         ex_select = 0
                         continue
+
+                # コマンドの実行
                 if mode == "FIGHT":
                     if ex_select == 0:
                         pass # 戦闘移行用if文
 
-                if mode == "ACT":
+                if mode == "ACT": # 行動関係
                     if ex_select == 0:
                         run_point += 20
                         mode = "avoid"
@@ -151,7 +174,7 @@ def main():
                         pg.draw.rect(fight_ex.surface, (0, 0, 0), (0, 0, 250, 50))
                         pg.draw.rect(ex_2.surface, (0, 0, 0), (0, 0, 250, 50))
 
-                if mode == "MERCY":
+                if mode == "MERCY": # 逃げる関係
                     if ex_select == 0:
                         pg.draw.rect(fight_ex.surface, (0, 0, 0), (0, 0, 250, 50))
                         pg.draw.rect(ex_2.surface, (0, 0, 0), (0, 0, 250, 50))
@@ -164,7 +187,7 @@ def main():
                         fight_ex.blit(screen,(230, 230))
                         ex_2.blit(screen,(230, 280))
 
-                    if ex_select == 1 and run_point >= 50:
+                    if ex_select == 1 and run_point >= 50: # 逃がす
                         pg.draw.rect(fight_ex.surface, (0, 0, 0), (0, 0, 250, 50))
                         pg.draw.rect(ex_2.surface, (0, 0, 0), (0, 0, 250, 50))
                         fight_ex.str = "enemy run away"
@@ -177,9 +200,11 @@ def main():
                         ex_2.blit(screen,(230, 280))
 
             if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE and mode != "standard":
-                mode = "standard"
-                fight_ex.color_update(white)
-                ex_2.color_update(white)
+                # escapeを押したらコマンド選択画面に戻る
+                if mode != "avoid": # 戦闘中ではないとき
+                    mode = "standard"
+                    fight_ex.color_update(white)
+                    ex_2.color_update(white)
             if event.type == pg.KEYDOWN and event.key == pg.K_UP:
                 if mode  == "MERCY" or mode == "ACT":
                     if ex_select == 0:
@@ -227,7 +252,6 @@ def main():
             ex_2.update(screen, flag=False)
 
         if mode == "standard":
-            # screen.blit(sikaku1, (200, 200))
             pg.draw.rect(fight_ex.surface, (0, 0, 0), (0, 0, 250, 50))
             pg.draw.rect(ex_2.surface, (0, 0, 0), (0, 0, 250, 50))
 
